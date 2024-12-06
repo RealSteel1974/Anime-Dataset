cd0d3c70ed1b5d3b58c4aaf6d1091d0941c3b48e
Onkar Pawar <92322074+RealSteel1974@users.noreply.github.com>
2024-12-06 18:01:16 +0530

    Create Create_Table_Anime.sql

---
+++ Create_Table_Anime.sql
@@ -0,0 +1,10 @@
+#Create a Database in  RDS and then create a table to load the csv file using any client
+CREATE TABLE anime (
+    user_id INT,
+    username VARCHAR(255),
+    anime_id INT,
+    anime_title VARCHAR(255),
+    rating INT
+);
+
+
